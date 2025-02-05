54d2c4650e304d4bd29819d34bdbe5de74ca0d56
madgrog <madgrog@gmail.com>
2025-02-05 23:24:59 +0100

    _moved

--- pec_manager/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import fetchmail
-from . import helpdesk_ticket
-from . import helpdesk
-from . import mail_alias
-from . import mail_mail
-from . import mail_message
-from . import mail_thread
--- pec_manager/models/fetchmail.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class FetchmailServer(models.Model):
-    """Incoming POP/IMAP mail server account"""
-
-    _inherit = 'fetchmail.server'
-    is_pec = fields.Boolean("Is PEC?", default=False)
