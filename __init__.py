2d79b410e08ac59cf88a74ec827f8c4944229996
madgrog <madgrog@gmail.com>
2025-07-31 15:31:56 +0200

    Uninstall hook cleaned up

--- __init__.py
+++ __init__.py
@@ -5,24 +5,9 @@ from . import wizard
 
 def _restore_helpdesk(env):
     """
-        This method restores original Enterprise Helpdesk:
-        - app icon
-        # - two record rules customized to support group based access to Helpdesk Teams and Tickets
+        This method restores original Enterprise Helpdesk
     """
-    # env = api.Environment(cr, SUPERUSER_ID, {})
     wid = env['ir.ui.menu'].search([('web_icon', '=', 'pec_manager,static/description/menu_icon.png')])
     for record in wid:
         record.write({'web_icon': 'helpdesk,static/description/icon.png'})
-    # cr.execute("""UPDATE ir_rule SET domain_force = '[''|'',
-    #                                         (''privacy_visibility'', ''!='', ''invited_internal''),
-    #                                         (''message_partner_ids'', ''in'', [user.partner_id.id])
-    #                                     ]'
-    #                 WHERE name = 'Helpdesk User'""")
-    # cr.execute("""UPDATE ir_rule SET domain_force = '[''|'',
-    #                                     ''|'',
-    #                                         (''team_id.privacy_visibility'', ''!='', ''invited_internal''),
-    #                                         (''team_id.message_partner_ids'', ''in'', [user.partner_id.id]),
-    #                                         (''message_partner_ids'', ''in'', [user.partner_id.id]),
-    #                                     ]'
-    #                 WHERE name = 'Helpdesk Ticket User'""")
-    print("PEC Manager module uninstalled!")
+    print("Restored stock Helpdesk menu icon!")
