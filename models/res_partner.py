7a7d7adcbd9ecaaf29cc632a5809d30dbef1b0a2
madgrog <madgrog@gmail.com>
2026-04-03 11:25:38 +0200

    new contact created as pec type

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,4 +1,7 @@
-from odoo import fields, models, _
+from odoo import fields, models, _, api
+from odoo.addons import pec_manager
+from odoo.addons.l10n_cl_edi.models import fetchmail_server
+
 
 class Partner(models.Model):
     _inherit = 'res.partner'
@@ -11,3 +14,11 @@ class Partner(models.Model):
              "- Other: Other address for the company (e.g. subsidiary, ...).\n"
              "- PEC Address: Italian Certified E-mail Address."
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        fetchmail_server_id = self.env.context.get('default_fetchmail_server_id')
+        if self.env['fetchmail.server'].search([('id', '=', fetchmail_server_id)]).is_pec:
+            res['type'] = 'pec'
+        return res
